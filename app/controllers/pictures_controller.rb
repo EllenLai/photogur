9b1125da68dd53a613fd6a63227e4a425941092d
EllenLai <miss.ellenlai@gmail.com>
2014-01-27 16:57:30 -0500

    Refactoring Step #4 to use dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,26 +1,44 @@
 class PicturesController < ApplicationController
- 
-  def picture0
-    @picture = {
-      :title  => "The old church on the coast of White sea",
-      :artist => "Sergey Ershov",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
+
+  def index
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
   end
 
-  def picture1
-    @picture = {
-      :title  => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-    }
+  def show
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
   end
 
-  def picture2
-    @picture = {
-      :title  => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
-  end
 end
