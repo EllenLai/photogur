0b6c5528b16fd64f5c41dea7f4319a76edc6906b
EllenLai <miss.ellenlai@gmail.com>
2014-01-27 17:57:26 -0500

    Adding feature to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Photogur::Application.routes.draw do
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
 
   get 'pictures/:id' => 'pictures#show'
+
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
