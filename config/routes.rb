ce54a7afc7db7fb724da654744cc1c22158a19ab
EllenLai <miss.ellenlai@gmail.com>
2014-01-27 18:03:13 -0500

    Refeactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Photogur::Application.routes.draw do
+  resources :pictures
   root :to => "pictures#index"
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => "pictures#create" # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "picture"
-
-  root :to => "pictures#index"
-
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
