a201c0d5be84ae485dfd80438f825ca2216e54c1
EllenLai <miss.ellenlai@gmail.com>
2014-01-27 17:04:19 -0500

    Adding feature to create new picture using HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
 
-   get 'pictures/:id' => 'pictures#show', as: "picture"
+  post 'pictures' => "pictures#create" # this is a new line of code
+  get 'pictures/new' => 'pictures#new' # this is also a new line of code
+
+  get 'pictures/:id' => 'pictures#show'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
